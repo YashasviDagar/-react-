34e4ff84bddac07da8ceeb50f307773a530a2d45
Yashasvi Dagar <dagaryashu2046@gmail.com>
2026-03-06 19:36:44 +0530

    CUSTOM REACT AND LEARNING REACTELEMENTS

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,14 +1,24 @@
 import Practice from "./prac";
 function App() {
 
+  const username = "chai aur code"
   return (
-    //here <> is a fragment as jsx needs to return a single element we return this within many elements.
+    // //here <> is a fragment as jsx needs to return a single element we return this within many elements.
+    // <>
+    // <Practice/>/* funtion name*/
+    // <h1>HELLO</h1>
+    // <p>Test para</p>
+    // </>
+
+
     <>
-    <Practice/>/* funtion name*/
-    <h1>HELLO</h1>
-    <p>Test para</p>
-    </>
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+   </>
+   //here {username} is EVALUATED EXPRESSION means this will give the final output of the expression we cant use if else or something just the final answer
   )
+  
 }
 
 export default App
